99b53cf5601bc47a7c5a009109c6d75f75987e0e
Pratik Chauhan <pratikc.multiqos@gmail.com>
2022-06-17 15:48:17 +0530

    dsf

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta property="og:image:width" key="og-image_width" content="1200" />
         <meta property="og:image:height" key="og-image_height" content="630" />
-
+        <meta property="og:image:width" key="og-image_width" content="1200" />
+        <meta property="og:image:height" key="og-image_height" content="630" />
         <meta
           property="og:image"
           key="og-image"
